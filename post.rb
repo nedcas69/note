c2b0de842282fb9fe245c95a7a962c3dcb1e2d67
Shax <nedcas69@gmail.com>
2022-04-14 12:26:40 +0500

    class subclass

---
+++ post.rb
@@ -0,0 +1,30 @@
+class Post
+    def initialize
+        @created_at = Time.now
+        @text = nil
+    end
+    
+    def read_from_console
+        # todo
+    end
+
+    def to_string
+        # todo
+    end
+
+    def file_path
+        current_path = File.dirname(__FILE__)
+        file_name = @created_at.strftime("#{self.class.name}_%Y-%m-%d_%H-%m-%s.txt")
+        return current_path + "/" + file_name
+    end
+
+    def save
+        file = File.new(file_path, "w:UTF-8")
+
+        for item in to_strings do
+            file.puts(item)
+        end
+
+        file.close
+    end
+end
