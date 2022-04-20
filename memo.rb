65c29de7a57b8d79f749d1bb92efc0f6b6d5fab0
Shax <nedcas69@gmail.com>
2022-04-20 09:14:35 +0500

    1

--- memo.rb
+++ memo.rb
@@ -1,6 +1,13 @@
 class Memo < Post
     def read_from_console
-        
+        puts "Новая заметка(всё, что пишите до строчки \"end\":"
+        @text = []
+        line = nil
+        while line != nil do
+            line = STDIN.gets.chomp
+            @text << line
+        end
+        @text.pop
     end
 
     def to_string
